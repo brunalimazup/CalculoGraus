4c627657f42e1d94c14211a67e7297eb72c40db1
Bruna <bruna.lima@zup.com.br>
2019-05-16 20:43:32 -0300

    Serve para treinar o java

---
+++ src/exercicio3/graus.java
@@ -0,0 +1,15 @@
+package exercicio3;
+import java.util.Scanner;
+
+public class graus {
+	public static void main(String[] args) {
+		double tempC = 35.5;
+		double f;
+		f = (9*tempC+160)/5;
+		System.out.println("Resultado é:" + f);
+		
+		
+		
+
+}
+}
